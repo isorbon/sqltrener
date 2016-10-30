1ad56ed1f0de2586f82a1833d8b23219d8608e42
isorbon <clogsj@gmail.com>
2016-10-30 21:27:37 +0300

    boot

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'altair#home'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users #,only:[:new]
-  match '/signup',  to: 'users#new', via: [:get, :post]
-  match '/signin',   to: 'sessions#new', via:   [:get, :post]
+  match '/signup',  to: 'users#new', via: :post
+  match '/signin',   to: 'sessions#new', via: :post
+  match '/signup',  to: 'users#new', via: :get
+  match '/signin',   to: 'sessions#new', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
