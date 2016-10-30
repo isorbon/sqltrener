80040efb957797af4a7da3a8340e15b8f76ba63a
isorbon <clogsj@gmail.com>
2016-10-30 11:12:01 +0300

    good

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'altair/home'
+  get 'sessions/new'
 
-  get 'altair/help'
-
-  get 'altair/about'
-
-  get 'altair/contact'
-
-  get 'altair/theory'
+  resources :users
+  root to: 'altair#home'
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new',via: [:get, :post]
+  match '/signin',   to: 'sesions#new',via: [:get, :post]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
